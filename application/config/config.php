96d038ec43b47158957380f83096a39bdc1b249b
kumar31 <kumarappan@smaatapps.com>
2016-07-07 18:01:57 +0530

    Update config.php

--- application/config/config.php
+++ application/config/config.php
@@ -14,7 +14,7 @@
 | path to your installation.
 |
 */
-$config['base_url']	= 'https://justfit.herokuapp.com/';
+$config['base_url']	= 'http://staf.herokuapp.com/';
 
 /*
 |--------------------------------------------------------------------------
