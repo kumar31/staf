f079cc824458a20f68314976e553377733bd4dad
kumar31 <kumarappan@smaatapps.com>
2016-07-30 17:18:43 +0530

    Update test_smtp_basic.php

--- PHPMailer/examples/test_smtp_basic.php
+++ PHPMailer/examples/test_smtp_basic.php
@@ -20,19 +20,19 @@ $body             = file_get_contents('contents.html');
 $body             = eregi_replace("[\]",'',$body);
 
 $mail->IsSMTP(); // telling the class to use SMTP
-$mail->Host       = "mail.yourdomain.com"; // SMTP server
+$mail->Host       = "mail.smaatapps.com"; // SMTP server
 $mail->SMTPDebug  = 2;                     // enables SMTP debug information (for testing)
                                            // 1 = errors and messages
                                            // 2 = messages only
 $mail->SMTPAuth   = true;                  // enable SMTP authentication
-$mail->Host       = "mail.yourdomain.com"; // sets the SMTP server
-$mail->Port       = 26;                    // set the SMTP port for the GMAIL server
-$mail->Username   = "yourname@yourdomain"; // SMTP account username
-$mail->Password   = "yourpassword";        // SMTP account password
+$mail->Host       = "mail.smaatapps.com"; // sets the SMTP server
+$mail->Port       = 25;                    // set the SMTP port for the GMAIL server
+$mail->Username   = "dhatshin@smaatapps.com"; // SMTP account username
+$mail->Password   = "Dhatshin123$%";        // SMTP account password
 
-$mail->SetFrom('name@yourdomain.com', 'First Last');
+$mail->SetFrom('dhatshin@smaatapps.com', 'First Last');
 
-$mail->AddReplyTo("name@yourdomain.com","First Last");
+$mail->AddReplyTo("dhatshin@smaatapps.com","First Last");
 
 $mail->Subject    = "PHPMailer Test Subject via smtp, basic with authentication";
 
@@ -40,7 +40,7 @@ $mail->AltBody    = "To view the message, please use an HTML compatible email vi
 
 $mail->MsgHTML($body);
 
-$address = "whoto@otherdomain.com";
+$address = "kumarappan@smaatapps.com";
 $mail->AddAddress($address, "John Doe");
 
 $mail->AddAttachment("images/phpmailer.gif");      // attachment
