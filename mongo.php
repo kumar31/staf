e58eedd8fe6bbbcb1821e3445e98b7942edc43ea
kumar31 <kumarappan@smaatapps.com>
2017-01-24 13:05:52 +0530

    Update mongo.php

--- mongo.php
+++ mongo.php
@@ -1,7 +1,14 @@
 <?php
    // connect to mongodb
-   $m = new MongoClient("mongodb://smaatapps:smaat123@ds127399.mlab.com:27399/heroku_5tt1944k");
 	
    echo "Connection to database successfully";
-   
+   $uri = "mongodb://smaatapps:smaat123@ds127399.mlab.com:27399/heroku_5tt1944k";
+
+/* 
+ * We recommend explictly configuring a connection timeout (see tips & tricks
+ * below). Specify the replica set name to avoid connection errors.
+ */ 
+
+$client = new MongoClient($uri);
+ echo "Connection to database successfully";
 ?>
