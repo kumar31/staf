edc6dbebae12252ab9a31450ce4e6d9d03892e98
kumar31 <kumarappan@smaatapps.com>
2017-01-24 13:02:00 +0530

    Update mongo.php

--- mongo.php
+++ mongo.php
@@ -1,6 +1,6 @@
 <?php
    // connect to mongodb
-   $m = new MongoClient();
+   $m = new MongoClient("mongodb://smaatapps:smaat123@ds127399.mlab.com:27399/heroku_5tt1944k");
 	
    echo "Connection to database successfully";
    
